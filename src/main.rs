e7365a33c665b670e04bb5af71a90e5e79fb24a1
David Weis <davidmichaelweis@gmail.com>
2019-09-06 23:44:56 +0100

    Add simple lunar module drawing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use quicksilver::{
-    geom::{Line, Rectangle, Shape, Transform, Vector},
+    geom::{Line, Rectangle, Circle, Shape, Transform, Vector, Scalar},
     graphics::{
         Background::{Col, Img},
-        Color, Font, FontStyle,
+        Background,
+        Color, Font, FontStyle, Drawable, Mesh
     },
     input::Key,
     lifecycle::{run, Asset, Settings, State, Window},
@@ -28,11 +29,66 @@ impl Map {
     }
 }
 
-struct Game {
-    font: Asset<Font>,
+struct LunarModule {
     velocity: Vector,
     position: Vector,
-    angle: i32,
+    attitude: f32,
+}
+
+impl LunarModule {
+    fn new(position: Vector) -> LunarModule {
+        LunarModule {
+            velocity: Vector::new(0.0, 0.0),
+            position,
+            attitude: 0.0,
+        }
+    }
+
+    fn apply_thrust(&mut self) {
+        self.velocity = self
+                .velocity
+                .translate(Transform::rotate(self.attitude) * (Vector::new(0, -30) / 60.0));
+    }
+
+    fn apply_gravity(&mut self) {
+        self.velocity = self.velocity.translate(Vector::new(0, 10) / 60.0);
+    }
+
+    fn tick_position(&mut self) {
+        self.position = self.position.translate(self.velocity / 60.0);
+    }
+}
+
+impl Drawable for LunarModule {
+    fn draw<'a>(&self, mesh: &mut Mesh, bkg: Background<'a>, trans: Transform, z: impl Scalar) {
+        let top = Circle::new(self.position + (Transform::rotate(self.attitude) * Vector::new(0, -10)), 5);
+        let top_black = Circle::new(self.position + (Transform::rotate(self.attitude) * Vector::new(0, -10)), 4);
+
+        let main_rect_top_left = self.position + Vector::new(-5, -5);
+        let main_rect = Rectangle::new(main_rect_top_left, Vector::new(10, 10));
+        let black_rect_top_left = self.position + Vector::new(-4, -4);
+        let black_rect = Rectangle::new(black_rect_top_left, Vector::new(8, 8));
+
+        // feet
+
+        let bottom_left = self.position + (Transform::rotate(self.attitude) * Vector::new(-5, 5));
+        let left_leg_base = Line::new(bottom_left, bottom_left + (Transform::rotate(self.attitude) * Vector::new(-5, 5)));
+
+        let bottom_right = self.position + (Transform::rotate(self.attitude) * Vector::new(5, 5));
+        let right_leg_base = Line::new(bottom_right, bottom_right + (Transform::rotate(self.attitude) * Vector::new(5, 5)));
+
+        main_rect.draw(mesh, bkg, Transform::rotate(self.attitude) * trans, z);
+        black_rect.draw(mesh, Col(Color::BLACK), Transform::rotate(self.attitude) * trans, z);
+        top.draw(mesh, bkg, trans, z);
+        top_black.draw(mesh, Col(Color::BLACK), trans, z);
+        left_leg_base.draw(mesh, bkg, trans, z);
+        right_leg_base.draw(mesh, bkg, trans, z);
+    }
+}
+
+struct Game {
+    font: Asset<Font>,
+    lunar_module: LunarModule,
     map: Asset<Vec<Line>>,
 }
 
@@ -52,28 +108,26 @@ impl State for Game {
 
         Ok(Game {
             font: font,
-            velocity: Vector::new(0, 0),
-            position: Vector::new(400, 300),
-            angle: 0,
+            lunar_module: LunarModule::new(Vector::new(400, 300)),
             map: map,
         })
     }
 
     fn update(&mut self, window: &mut Window) -> Result<()> {
         if window.keyboard()[Key::Left].is_down() || window.keyboard()[Key::A].is_down(){
-            self.angle = self.angle - 3;
+            self.lunar_module.attitude -= 3.0;
         }
         if window.keyboard()[Key::Right].is_down() || window.keyboard()[Key::D].is_down() {
-            self.angle = self.angle + 3;
+            self.lunar_module.attitude += 3.0;
         }
         if window.keyboard()[Key::Up].is_down() || window.keyboard()[Key::W].is_down(){
-            self.velocity = self
-                .velocity
-                .translate(Transform::rotate(self.angle) * (Vector::new(0, -30) / 60.0));
+            self.lunar_module.apply_thrust();
         }
-        self.velocity = self.velocity.translate(Vector::new(0, 10) / 60.0);
-        self.position = self.position.translate(self.velocity / 60.0);
-
+        if window.keyboard()[Key::Space].is_down() {
+            self.lunar_module = LunarModule::new(Vector::new(400, 300))
+        }
+        self.lunar_module.apply_gravity();
+        self.lunar_module.tick_position();
         Ok(())
     }
 
@@ -87,18 +141,10 @@ impl State for Game {
             }
             Ok(())
         })?;
-
-        let top_left = self.position - Vector::new(-5, -5);
-
-        let player_model = Rectangle::new(top_left, Vector::new(10, 10));
-        window.draw_ex(
-            &player_model,
-            Col(Color::WHITE),
-            Transform::rotate(self.angle),
-            10,
-        );
-        let horizontal = self.velocity.x;
-        let vertical = self.velocity.y;
+        window.draw(&self.lunar_module, Color::WHITE);
+        
+        let horizontal = self.lunar_module.velocity.x;
+        let vertical = self.lunar_module.velocity.y;
         self.font.execute(move |font| {
             let style = FontStyle::new(20.0, Color::WHITE);
             let text = format!("Horizontal: {:.0}\nVertical: {:.0}", horizontal, vertical);
